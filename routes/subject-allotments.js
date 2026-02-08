a621f06ce7040c380bbcf9afcd25bb22a90aa184
Ramu Kavalluri <sairamhanuman85@gmail.com>
2026-02-08 15:17:04 +0530

    Add branch information to allotments query

--- routes/subject-allotments.js
+++ routes/subject-allotments.js
@@ -6,17 +6,19 @@ module.exports = (promisePool) => {
     router.get('/', async (req, res) => {
         try {
             const [rows] = await promisePool.query(`
-                SELECT a.allotment_id,
-                       s.syllabus_code,
-                       s.subject_name,
-                       b.batch_name,
-                       sec.section_name,
-                       CONCAT(st.employee_id, ' - ', st.full_name) AS faculty_name
-                FROM subject_faculty_allotment a
-                JOIN subject_master s ON a.subject_id = s.subject_id
-                JOIN batch_master b ON a.batch_id = b.batch_id
-                JOIN section_master sec ON a.section_id = sec.section_id
-                JOIN staff_master st ON a.staff_id = st.staff_id
+             SELECT a.allotment_id,
+       s.syllabus_code,
+       s.subject_name,
+       b.batch_name,
+       sec.section_name,
+       CONCAT(br.branch_code, ' - ', st.employee_id, ' - ', st.full_name) AS faculty_name
+FROM subject_faculty_allotment a
+JOIN subject_master s ON a.subject_id = s.subject_id
+JOIN batch_master b ON a.batch_id = b.batch_id
+JOIN section_master sec ON a.section_id = sec.section_id
+JOIN staff_master st ON a.staff_id = st.staff_id
+JOIN branch_master br ON a.branch_id = br.branch_id
+
                 WHERE a.deleted_at IS NULL
                 ORDER BY a.allotment_id DESC
             `);
